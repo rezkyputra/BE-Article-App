4c9a1c1f52097d93fd267edc86edc68dd6464f39
rezkyputra <rezky.putra46@gmail.com>
2025-05-07 13:27:01 +0800

    ulang

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api/v1');
-  app.enableCors()
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
